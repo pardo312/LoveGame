f5341ece854902b7e7150aa478c099902bd266e2
Jiufen <42390277+pardo312@users.noreply.github.com>
2021-02-05 10:37:53 -0500

    Update ApplicationModel.cs

--- Assets/Scripts/ApplicationModel.cs
+++ Assets/Scripts/ApplicationModel.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class ApplicationModel : MonoBehaviour
 {
-//COLP Daniel10$
+
     static public Vector3 posicionDelJugador = new Vector3(0,-2.32f,0);
     static public bool playerCanMove = true;
     static public bool startDialog = true;
